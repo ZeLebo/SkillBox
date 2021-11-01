5559ead78f33971d5cc1e8c49b40d70e176a9e66
= <sartakov.sasha@icloud.com>
2021-11-01 23:34:44 +0700

    Changes

--- task_6.go
+++ task_6.go
@@ -18,16 +18,32 @@ func taskSecond() {
     var a, b int
     fmt.Println("Put the numbers:")
     fmt.Scanf("%d %d", &a, &b)
-    for a < a + b + 1 {
+    for i := 0 ; i < b; i++ {
         a++
     }
-    fmt.Println("Here's the sum:\n", a)
+    fmt.Println("Here's the sum:", a)
 }
 
 func taskThird() {
+    var price, discount float32
+    fmt.Println("Put the price and discount:")
+    fmt.Scanf("%f %f%", &price, &discount)
+    discount /= 100
+    if ( discount > 0.3 ) {
+        discount = 0.3
+    } else if (discount < 0.0 ){
+        discount = 0.0
+    }
+    if (discount * price) > 2000 {
+        price -= 2000
+    } else {
+        price *= ( 1.0 - discount )
+    }
+    fmt.Println("Here's how much you have to pay:", price)
 }
 
 func taskForth(){
+    fmt.println()
 }
 
 func taskFifth() {
