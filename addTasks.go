e1871be094b89fe8f7db46c190f08d803952c50d
= <sartakov.sasha@icloud.com>
2021-11-30 01:19:22 +0700

    task 13

--- addTasks.go
+++ addTasks.go
@@ -1,8 +1,15 @@
 package main
+
 import (
-    "fmt";
-    "strings";
+    "bufio"
+    "fmt"
+    "io/ioutil"
+    "math/rand"
+    "os"
     "sort"
+    "strconv"
+    "strings"
+    "time"
 )
 
 func substring(){
@@ -24,18 +31,102 @@ func stringSort(){
     }
 }
 
-/*
-1) Написать простой генератор IP адрессов
-2) Найти произведение 2 больших натуральных чисел ( N max = 50 )
-3) Вывести элементы строки в которой содержаться числа
-4) Передача fileName через параметры запуска программы
-*/
-
 func IPgenerator() {
-    math.seed(1)
+    fmt.Println("IP generator")
+    var ip string
+    rand.Seed(time.Now().UnixMilli())
+    for i := 0; i < 4; i++ {
+        ip += strconv.Itoa(rand.Int() % 999)
+        if i != 3 {
+            ip += "."
+        }
+    }
+    fmt.Println("Your IP is", ip)
+}
+
+func runesToString(runes [100]rune) (outString string) {
+    var index int
+    for i := 0; i < 100; i++ {
+        if runes[i] != '0' - 48 {
+            index = i
+            break
+        }
+    }
+
+    for i := index; i < 100; i++ {
+        outString += string(runes[i] + 48)
+    }
+    return outString
+}
+
+func longMultiply() {
+    fmt.Println("Give me 2 numbers to multiply")
+    var num1, num2 string
+    fmt.Scanf("%s %s", &num1, &num2)
+    var (
+        first, second [50]rune
+        result [100]rune
+    )
+    for i := 0; i < len(num1); i++ {
+        first[49 - i] = rune(num1[len(num1) - i - 1]) - 48
+    }
+    for i := 0; i < len(num2); i++ {
+        second[49 - i] = rune(num2[len(num2) - i - 1]) - 48
+    }
+    for i := 0; i < 25; i++ {
+        first[i], first[49 - i] = first[49 - i], first[i]
+        second[i], second[49 - i] = second[49 - i], second[i]
+    }
+
+    for i := 0; i < 50 ; i++ {
+        for j := 0; j < 50; j++ {
+            result[j + i] += first[j] * second[i]
+        }
+    }
+
+    for i := 0; i < 99; i++ {
+        result[i + 1] += result[i] / 10
+        result[i] %= 10
+    }
+    for i := 0; i < 50; i++ {
+        result[i], result[99 - i] = result[99 - i], result[i]
+    }
+    answer, _ := strconv.Atoi(runesToString(result))
+    fmt.Println(answer)
+}
+
+func checkString(str string, subs ...string) bool {
+    for _, sub := range subs {
+        if strings.Contains(str, sub) {
+            return true
+        }
+    }
+    return false
+}
+
+func numberContatining() {
+    fmt.Println("Numbers finding")
+    line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+    stringToFind := strings.Split(line, " ")
+    for i := range(stringToFind) {
+        if checkString(stringToFind[i], "0", "1", "2", "3", "4", "5", "6", "7", "8", "9") {
+            fmt.Println(stringToFind[i])
+        }
+    }
+}
+
+func fileNameChanging(fileName string) {
+    file, _ := os.Create(fileName)
+    defer file.Close()
+    stringToWrite := "Hello world!"
+    ioutil.WriteFile(fileName, []byte(stringToWrite), 755)
 }
 
 func main() {
-    substring()
-    stringSort()
+    //substring()
+    //stringSort()
+    //IPgenerator()
+    //longMultiply()
+    //numberContatining()
+    //fileNameChanging(os.Args[1])
 }
