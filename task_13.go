38e23e5d2aa3315e532b2d9e151a84db98909bc6
= <sartakov.sasha@icloud.com>
2021-11-29 22:49:39 +0700

    task 13

---
+++ task_13.go
@@ -0,0 +1,34 @@
+package main
+import "fmt"
+
+func mySwap(a, b *int) {
+	tmp := *a
+	*a = *b
+	*b = tmp
+}
+
+func add(a, b int) int {
+	var result int
+	if a > b {
+		mySwap(&a, &b)
+	}
+	for i := a; i < b + 1; i++ {
+		if i % 2 == 0 {
+			result += i
+		}
+	}
+	return result
+}
+
+func main() {
+	var a, b int
+	fmt.Println("The first task")
+	fmt.Println("Put here 2 numbers:")
+	fmt.Scanf("%d %d", &a, &b)
+	fmt.Println(add(a, b))
+
+	fmt.Println("The second task")
+	fmt.Println("Before swap:", a, b)
+	mySwap(&a, &b)
+	fmt.Println("After swap:", a, b)
+}
