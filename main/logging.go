afbdccbbfe6e819bc87d7cb38e81561d7fd2b09d
= <sartakov.sasha@icloud.com>
2021-12-06 18:12:01 +0700

    new files

--- main/logging.go
+++ main/logging.go
@@ -1,41 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 
-    log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 func runAndWait() int {
-    time.Sleep(time.Millisecond * 1)
-    return 10
+	time.Sleep(time.Millisecond * 1)
+	return 10
 }
 
 func init() {
-    log.SetFormatter(&log.JSONFormatter{})
+	log.SetFormatter(&log.JSONFormatter{})
 
-    log.SetLevel(log.WarnLevel)
+	log.SetLevel(log.WarnLevel)
 }
 
 func main() {
-    for i := 0; i < 100; i++ {
+	defer func() {
+		fmt.Println("The code is done")
+	}()
 
-        a := runAndWait()
-        log.Info("runAndWait finished...")
-        log.Infof("result = %d", a)
+	for i := 0; i < 100; i++ {
 
-        if i > 90 {
-            log.WithFields(log.Fields{
-                "i": i,
+		a := runAndWait()
 
-            }).Warn("closer to the edge")
-        }
+		log.Info("runAndWait finished...")
+		log.Infof("result = %d", a)
 
-        if i == 99 {
-            log.Fatal("reached 100")
-        }
-    }
-
-    fmt.Println("done")
+		if i > 90 {
+			log.WithFields(log.Fields{
+				"i": i,
+			}).Warn("closer to the edge")
+		}
+	}
 }
