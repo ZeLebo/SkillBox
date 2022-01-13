b66ba51cf62487f664b987a4bd7fe91a68abdd0b
= <sartakov.sasha@icloud.com>
2022-01-13 22:03:10 +0700

    fibonacki benchmark

--- Additional/fibonachi_test.go
+++ Additional/fibonachi_test.go
@@ -1,52 +1,47 @@
 package main
 
-import "fmt"
+import (
+		"fmt";
+		"testing"
+)
 
-// write Benchmark
+func Benchmark_recursion()
 
-func evenCounter() (int, int) {
-	var (
-		arr [10]int
-		even int
-	)
-	for i := 0; i < 10; i++ {
-		fmt.Scan(&arr[i])
+func recursion (n int) int {
+	result := 0
+	if n < 1 {
+		result = 0
+	} else if n == 1 {
+		result = 0
+	} else if n == 2 {
+		result = 1
+	} else {
+		result = recursion(n-1) + recursion(n-2)
 	}
-	for i := 0; i < 10; i++ {
-		if arr[i] % 2 == 0 {
-			even++
+	return result
+}
+
+func addition(n int) int {
+	result := 0
+	if n == 1 {
+		result = 0
+	} else if n == 2 {
+		result = 1
+	} else {
+		num1, num2 := 0, 1
+		for i := 2; i < n; i++ {
+			result = num2 + num1
+			num1 = num2
+			num2 = result
 		}
 	}
-	return even, 10 - even
-}
-
-func reverseArr(arr []int) []int {
-	length := len(arr)
-	for i := 0; i < length / 2; i++ {
-		arr[i], arr[length - i - 1] = arr[length - i - 1], arr[i]
-	}
-	return arr
-}
-
-func secondTask() []int {
-	var arr []int
-	var tmp int
-
-	_, err := fmt.Scanf("%d", &tmp)
-	for err == nil {
-		arr = append(arr, tmp)
-		_, err = fmt.Scanf("%d", &tmp)
-	}
-
-	return reverseArr(arr)
+	return result
 }
 
 func main() {
-	// fmt.Println("The first task\nGimme 10 numbers")
-	// fmt.Println(evenCounter())
-	// fmt.Println("The second task")
-	result := secondTask()
-	for i := 0; i < len(result); i++ {
-		fmt.Printf("%d ", result[i])
-	}
+	var n int
+	fmt.Scan(&n)
+
+	fmt.Println(addition(n))
+	fmt.Println(recursion(n))
 }
