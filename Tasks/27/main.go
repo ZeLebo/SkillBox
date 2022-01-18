f5930cd5e817919c1fe3c6de64181e78b22bd388
= <sartakov.sasha@icloud.com>
2022-01-18 18:47:29 +0700

    the 27 task

--- Tasks/27/main.go
+++ Tasks/27/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-    "fmt"
-)
+import "fmt"
 
 type Student struct {
     name string
@@ -18,18 +16,16 @@ func newStudent(name string, age, grade int) *Student{
 }
 
 func main() {
-    var (
-        name string;
-        age, grade int;
-    )
+    var ( name string;
+        age, grade int )
     storage := make(map[string] *Student)
 
-    _, err := fmt.Scanf("%s %d %d", &name, &age, &grade)
-    for  err == nil {
+    size, err := fmt.Scanf("%s %d %d", &name, &age, &grade)
+    for  err == nil && size == 3 {
         storage[name] = newStudent(name, age, grade)
-        _, err = fmt.Scanf("%s %d %d", &name, &age, &grade)
+        size, err = fmt.Scanf("%s %d %d", &name, &age, &grade)
     }
-
+    fmt.Println("Students from storage:")
     for i := range storage {
         fmt.Printf("Name: %s, Age: %d, Grade: %d\n", i, storage[i].age, storage[i].grade)
     }
