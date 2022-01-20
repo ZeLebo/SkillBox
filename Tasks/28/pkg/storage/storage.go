59b62b06b20993678927f6b6d701594b92cfb13f
= <sartakov.sasha@icloud.com>
2022-01-20 21:01:46 +0700

    the 28 task

--- Tasks/28/pkg/storage/storage.go
+++ Tasks/28/pkg/storage/storage.go
@@ -2,10 +2,28 @@ package main
 
 import (
     "student/pkg/student"
+    "fmt"
+    "os"
 )
 
+func GetStudent() map[string] *student.Student {
+    var (
+        name string;
+        age, grade int
+    )
+    storage := make(map[string] *student.Student)
+
+    size, err := fmt.Fscanf(os.Stdin, "%s %d %d", &name, &age, &grade)
+    for  err == nil && size == 3 {
+        storage[name] = student.NewStudent(name, age, grade)
+        size, err = fmt.Fscanf(os.Stdin, "%s %d %d", &name, &age, &grade)
+    }
+
+    return storage
+}
+
 func main() {
-    storage := student.GetStudent()
+    storage := GetStudent()
     for i := range storage {
         storage[i].PrintInfo()
     }
