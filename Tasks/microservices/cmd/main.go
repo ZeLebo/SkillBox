db9116e9417bd9687742e10bcee8f3f2cd1cb2a6
ZeLebo <sartakov.sasha@icloud.com>
2022-08-03 22:14:07 +0800

    everyday I wake up

--- Tasks/microservices/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	micro "micro/pkg"
-)
-
-func main() {
-	micro.Test()
-}
--- Tasks/microservices/pkg/test.go
+++
@@ -1,9 +0,0 @@
-package micro
-
-import (
-	"fmt"
-)
-
-func Test() {
-	fmt.Println("The test func")
-}
